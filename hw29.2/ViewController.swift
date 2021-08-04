f666261add19560798968a3b28475721d06097de
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-08-04 14:39:44 +0300

    Initial Commit

---
+++ hw29.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw29.2
+//
+//  Created by Vahtee Boo on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
